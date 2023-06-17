74d60d2297a5f895c259131c4d6a928cb5ea41b0
ermelsu <emerson.silva.bezerra@gmail.com>
2023-06-16 23:45:04 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task  from './components/task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+ 
+      {/* lista de tarefas de hoje */}
+
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tarefas de Hoje</Text>
+          <View style={styles.items}>
+            {/* pra onde as tarefas vão */}
+            <Task Text={'Task 1'}/>
+            <Task Text={'Task 2'}/>
+          </View>
+        </View>
     </View>
   );
 }
@@ -13,8 +23,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
   },
 });
