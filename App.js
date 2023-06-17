e5271d2b37dd4c2b590dcf7ea6d6b576bd0f3d7b
ermelsu <emerson.silva.bezerra@gmail.com>
2023-06-17 00:28:11 -0300

    teste para add no terminal funciona

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Task  from './components/task';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task  from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+
+
+  const handleAddTask = () => {
+    console.log(task);
+
+  }
+
   return (
     <View style={styles.container}>
  
@@ -16,6 +25,22 @@ export default function App() {
             <Task Text={'Task 2'}/>
           </View>
         </View>
+
+
+
+
+        {/* escreva a terfa */}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style = {styles.writetasksWrapper}
+        >
+          <TextInput style={styles.input} placeholder='{Escreva uma tarefa}' value={task} onChangeText={text => setTask(text)} />
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
     </View>
   );
 }
@@ -36,4 +61,33 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writetasksWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addText: {},
 });
